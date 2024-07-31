1f8ecbc45082b6411cf558a97bd4604f6a668318
alexyai <alexyai@gmx.at>
2024-07-31 18:04:46 +0200

    Add flash controller class

---
+++ ESP32/src/flash_controller.h
@@ -0,0 +1,22 @@
+#ifndef FLASH_CONTROLLER_H
+#define FLASH_CONTROLLER_H
+
+#pragma once
+
+#include <cstring>
+#include <string>
+
+#include "nvs_flash.h"
+
+class Flash_controller {
+ public:
+  Flash_controller();
+  ~Flash_controller();
+
+  esp_err_t init();
+
+ private:
+  const std::string tag;
+};
+
+#endif  // FLASH_CONTROLLER_H
