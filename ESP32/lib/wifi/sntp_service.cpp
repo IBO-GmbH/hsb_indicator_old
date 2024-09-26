606f0d7c44d907e725b2542030b9223acefb5c6a
AlexanderBaranyaiIBO <alexander.baranyai@ibo.at>
2024-09-26 15:09:07 +0200

    update sntp init and add deinit

--- ESP32/lib/wifi/sntp_service.cpp
+++ ESP32/lib/wifi/sntp_service.cpp
@@ -2,11 +2,27 @@
 
 Sntp_service::Sntp_service() {}
 
-Sntp_service::~Sntp_service() {}
+Sntp_service::~Sntp_service() {
+  esp_err_t ret;
+  ret = this->deinit();
+  ESP_ERROR_CHECK(ret);
+}
 
-void Sntp_service::init() {
+esp_err_t Sntp_service::init() {
+  esp_err_t ret;
   setenv("TZ", "UTC0", 1);
   tzset();
   esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
-  esp_netif_sntp_init(&config);
+  config.start = false;  // start SNTP service explicitly
+  ret = esp_netif_sntp_init(&config);
+  if (ret != ESP_OK) {
+    return ret;
+  }
+  ret = esp_netif_sntp_start();
+  return ret;
+}
+
+esp_err_t Sntp_service::deinit() {
+  esp_netif_sntp_deinit();
+  return ESP_OK;
 }
