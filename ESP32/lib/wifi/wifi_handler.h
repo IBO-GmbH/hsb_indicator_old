eacc51f698dfa2c61380b1b99662f4e0013592fd
alexyai <alexyai@gmx.at>
2024-11-27 19:58:54 +0100

    update wifi handler

--- ESP32/lib/wifi/wifi_handler.h
+++ ESP32/lib/wifi/wifi_handler.h
@@ -12,36 +12,40 @@
 
 #define DEFAULT_SCAN_LIST_SIZE 10
 
-#define EXAMPLE_ESP_MAXIMUM_RETRY 5
+#define MAXIMUM_WIFI_CONNECTION_RETRIES 5
 
 // Scan only non overlapping channels using Channel bitmap
 #define USE_SCAN_CHANNEL_BITMAP false
+#if USE_SCAN_CHANNEL_BITMAP
+#define CHANNEL_LIST_SIZE 3
+static uint8_t channel_list[CHANNEL_LIST_SIZE] = {1, 6, 11};
+#endif
 
 class Wifi_handler {
  public:
   Wifi_handler();
   ~Wifi_handler();
 
-  void event_handler(void *event_handler_arg, esp_event_base_t event_base,
-                     int32_t event_id, void *event_data);
+  void event_handler(void *event_handler_arg, const esp_event_base_t event_base,
+                     const int32_t event_id, const void *event_data);
 
   // initializes Wifi in Station Mode
   esp_err_t init_sta();
   esp_err_t deinit();
 
-  esp_err_t connect_to_wifi();
+  esp_err_t connect_to_wifi() const;
   esp_err_t connect_to_wifi(const std::string_view ssid,
                             const std::string_view password);
   esp_err_t set_ssid_and_pw(const std::string_view ssid,
                             const std::string_view password);
 
   // returns array or vector of scanned AP's up to the size of the array
-  esp_err_t scan(std::span<wifi_ap_record_t> ap_info);
+  esp_err_t scan(std::span<wifi_ap_record_t> ap_info) const;
   // prints scan data to the monitor
-  void log_ap_info(std::span<wifi_ap_record_t> ap_info) const;
+  void log_ap_info(const std::span<wifi_ap_record_t> ap_info) const;
   // check if ssid is contained in wifi records and returns true if found
-  bool ssid_in_records(std::span<wifi_ap_record_t> ap_info,
-                       std::string_view ssid);
+  bool ssid_in_records(const std::span<wifi_ap_record_t> ap_info,
+                       const std::string_view ssid) const;
 
 #ifdef USE_CHANNEL_BTIMAP
   static void array_2_channel_bitmap(const uint8_t channel_list[],
@@ -53,15 +57,16 @@ class Wifi_handler {
   // this function is used that a class member function can be used as event
   // handler function
   static void event_handler_static(void *event_handler_arg,
-                                   esp_event_base_t event_base,
-                                   int32_t event_id, void *event_data);
+                                   const esp_event_base_t event_base,
+                                   const int32_t event_id, void *event_data);
 
-  const std::string tag;
-  std::string m_ssid;
-  std::string m_password;
+ private:
+  const std::string m_tag = "wifi_handler";
+  std::string m_ssid = "";
+  std::string m_password = "";
   uint8_t m_retry_num = 0;
   bool m_connecting = false;
-  esp_netif_t *wifi_netif = NULL;
+  esp_netif_t *m_wifi_netif = NULL;
 };
 
 #endif  // WIFI_HANDLER_H
