6cc2f85f48e8b4d849e3913220134757168c76de
AlexanderGrathIBO <alexander.grath@ibo.at>
2025-02-12 13:53:32 +0100

    updated fonts

--- ESP32/lib/ui_library/fonts.h
+++ ESP32/lib/ui_library/fonts.h
@@ -7,6 +7,13 @@
 extern "C" {
 #endif
 
+extern const lv_font_t ui_font_clear_sans_regular_32;
+extern const lv_font_t ui_font_clear_sans_regular_36;
+extern const lv_font_t ui_font_clear_sans_regular_30;
+extern const lv_font_t ui_font_clear_sans_regular_14;
+extern const lv_font_t ui_font_clear_sans_regular_48;
+extern const lv_font_t ui_font_clear_sans_regular_38;
+extern const lv_font_t ui_font_clear_sans_regular_26;
 
 
 #ifdef __cplusplus
